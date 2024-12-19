cb6d3e67b8dcb8a56ffca692c68b499042e41dda
maxkanzas <maxkanz1991@gmail.com>
2024-12-19 18:37:25 +0400

    change WebSteps

--- src/test/java/WebSteps.java
+++ src/test/java/WebSteps.java
@@ -1,5 +1,11 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -32,5 +38,9 @@ public class WebSteps {
     @Step("Проверяем наличие Issue с номером {issue}")
     public void shouldSeeIssueWithNumber(int issue) {
         $(withText("#" + issue)).should(Condition.exist);
+    }
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeSkreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 }
